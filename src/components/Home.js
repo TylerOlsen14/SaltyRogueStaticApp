1fb859f219fde8d3aa13804c4a08e4a9a69520c4
TylerOlsen14 <Tyler.Olsen1@gmail.com>
2019-05-14 15:21:16 -0600

    made some forward movement on the front page & router

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,59 @@
 import React from 'react'
 // import React, { Component } from 'react'
+import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Home = () => {
   return (
     <div className="main">
-      <p>Home</p>
+      <h1>Home</h1>
+      <h3>Questions</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ipsa dignissimos voluptas quibusdam esse delectus suscipit? Architecto optio similique expedita repellat nihil. Veritatis facilis non deleniti mollitia quam quo voluptatem.</p>
+      <Form>
+        <FormGroup>
+          <Label for="exampleEmail">Contact Us</Label>
+          <p className="exampleEmail">Let us know what questions you might have. We will be happy to help you with whatever you need. Give your name and phonenumber and tell us which property you might be interested in. We will get back to you as soon as possible.</p>
+        </FormGroup>
+        <FormGroup>
+          <Label for="name">Name</Label>
+          <Input
+            type="name"
+            name="name"
+            id="name"
+            placeholder="with a placeholder"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleEmail">Email</Label>
+          <Input
+            type="Email"
+            name="Email"
+            id="exampleEmail"
+            placeholder="Email placeholder"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleNumber">Phone Number</Label>
+          <Input
+            type="number"
+            name="number"
+            id="exampleNumber"
+            placeholder="Phone Number placeholder"
+          />
+        </FormGroup>
+        <FormGroup check className="FormGroup">
+          <Label check className="checkbox">
+            <Input className="checkbox" type="checkbox" id="EmmaStreet" /> Emma Street
+            <Input className="checkbox" type="checkbox" id="LithiaPark" /> Lithia Park
+            <Input className="checkbox" type="checkbox" id="LehiSouth" /> Lehi South
+            <Input className="checkbox" type="checkbox" id="CottonwoodHeights" /> Cottonwood Heights
+            <Input className="checkbox" type="checkbox" id="AmericanFork" /> American Fork
+          </Label>
+        </FormGroup>
+        <FormGroup>
+          <Label for="exampleText">Mark your question here</Label>
+          <Input type="textarea" name="text" id="exampleText" placeholder="It can be as ridiculous as you want" />
+        </FormGroup>
+      </Form>
     </div>
   );
 }
