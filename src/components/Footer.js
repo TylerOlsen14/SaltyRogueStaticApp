e1833daba0cacdbc894bf422749a4cb6e816a589
Tyler Olsen <Tyler.Olsen1@gmail.com>
2019-05-22 14:35:38 -0600

    footer initial add

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import finn from '../assets/Finn.jpg'
 
 export class Footer extends Component {
   render() {
     return (
-      <div className="main">
-        
+      <div className="footer">
+        <img src={finn} alt="Captain Finn Web Design"/>
       </div>
     )
   }
